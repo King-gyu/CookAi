235dec385b836569f82b515b300d01bc48d2dfe9
RAON <ra0n@icloud.com>
2023-05-25 22:49:09 +0900

    feat/articles update

--- articles/serializers.py
+++ articles/serializers.py
@@ -1,17 +1,14 @@
 # 게시글 작성, 수정, 삭제, 조회를 위한 serializer
 
 from rest_framework import serializers
-from .models import Article, Comment
+from articles.models import Article, Comment
 
 class ArticleSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
     class Meta:
         model = Article
         fields = '__all__'
         
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField(source='author.username')
-    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
     class Meta:
         model = Comment
         fields = '__all__'
--- articles/urls.py
+++ articles/urls.py
@@ -1,19 +1,14 @@
 # 게시글 및 댓글 작성, 수정, 삭제, 상세보기, 목록보기에 대한 url을 정의
 
 from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    path('', views.ArticleCreateView.as_view(), name='article_create'),
-    path('<int:pk>/update/', views.ArticleUpdateView.as_view(), name='article_update'),
-    path('<int:pk>/delete/', views.ArticleDeleteView.as_view(), name='article_delete'),
-    path('<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
-    path('list/', views.ArticleListView.as_view(), name='article_list'),
+    path('', views.ArticleView.as_view(), name='article_view'),
+    path('<int:article_pk>/', views.ArticleDetailView.as_view(), name='article_detail_view'),
     
-    path('<int:pk>/comments/', views.CommentCreateView.as_view(), name='comment_create'),
-    path('<int:pk>/comments/<int:comment_pk>/', views.CommentUpdateView.as_view(), name='comment_update'),
-    path('<int:pk>/comments/<int:comment_pk>/delete/', views.CommentDeleteView.as_view(), name='comment_delete'),
-    path('<int:pk>/comments/<int:comment_pk>/detail/', views.CommentDetailView.as_view(), name='comment_detail'),
-    path('<int:pk>/comments/<int:comment_pk>/list/', views.CommentListView.as_view(), name='comment_list'),
+    path('comment/', views.CommentView.as_view(), name='comment_view'),
+    path('comment/<int:comment_pk>/', views.CommentDetailView.as_view(), name='comment_detail_view'),
+    
+    path('like/', views.LikeView.as_view(), name='like_view'),
 ]
